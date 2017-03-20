ca40678aec1a46bf5852288b3ba088bac6ff4d22
kevinguiot <contact@kevinguiot.fr>
2017-03-20 17:41:04 +0100

    Récupération des items.

--- exo_actu_ios/ViewController.m
+++ exo_actu_ios/ViewController.m
@@ -9,6 +9,15 @@
 #import "ViewController.h"
 #import "XMLReader.h"
 
+
+@interface Item : NSObject
+
+@property NSString *titre;
+@property NSString *description;
+
+@end
+
+
 @interface ViewController ()
 
 @end
@@ -17,18 +26,18 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-
+    
     // Récupération du contenu XML des actus le monde
     NSURL *urlActu = [NSURL URLWithString:@"http://www.lemonde.fr/rss/une.xml"];
     NSError* error;
     NSString *actuXML = [NSString stringWithContentsOfURL:urlActu encoding:NSASCIIStringEncoding error:&error];
     
-    //Parse du contenu dans un dictionnaire
+    // Parse du contenu dans un dictionnaire
     NSError *parseError = nil;
     NSDictionary *xmlDictionary = [XMLReader dictionaryForXMLString:actuXML error:&parseError];
     
-    // Print the dictionary
-    NSLog(@"%@", xmlDictionary);
+    // Récupération des items
+    NSDictionary *items = xmlDictionary[@"rss"][@"channel"][@"item"];
 }
 
 
