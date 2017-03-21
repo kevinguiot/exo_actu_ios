743b8f03e57a99c92313009d663ff0c1a178c80a
kevinguiot <contact@kevinguiot.fr>
2017-03-21 11:40:52 +0100

    Création d'une nouvelle classe lié au TableViewController.

---
+++ exo_actu_ios/ActuTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  ActuTableViewController.h
+//  exo_actu_ios
+//
+//  Created by GUIOT Kevin on 21/03/2017.
+//  Copyright © 2017 GUIOT Kevin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ActuTableViewController : UITableViewController
+
+@end
