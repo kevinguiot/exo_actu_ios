89783a1aabe252a8fb75a95249d6bd96d5767385
kevinguiot <contact@kevinguiot.fr>
2017-03-24 11:35:49 +0100

    Mise à jour des données du projet.

--- exo_actu_ios/ItemSelectedViewController.m
+++ exo_actu_ios/ItemSelectedViewController.m
@@ -16,27 +16,6 @@
 @synthesize description = _description;
 @synthesize link = _link;
 
-/*
-- (void)viewDidLoad {
-    [super viewDidLoad];
-    // Do any additional setup after loading the view.
-}
-
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
-}
-*/
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
-*/
-
 - (IBAction)moreButton:(UIButton *)sender {
     
     // On ouvre le lien sur Safari
